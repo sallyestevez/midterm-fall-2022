0eed6612146b6540999e00f05ca6ae0596f3e62b
Sally E <sme418@nyu.edu>
2022-10-18 09:16:49 -0400

    added empty pages and components

---
+++ pages/Home.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import axios from 'axios';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+  } from "react-router-dom";
---
+++ pages/Songs.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+  } from "react-router-dom";
+
+function Songs() {
+    return (
+        <div className="App">
+        </div>
+    );
+};
+
+export default Songs;
